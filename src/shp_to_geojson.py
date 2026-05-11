dcbfaf95fa459914f1edb900d104663b252fdc06
unhyepnhj <rdh0827@gmail.com>
2026-05-12 02:21:21 +0900

    Add residential analysis pipeline

--- src/shp_to_geojson.py
+++
@@ -1,7 +0,0 @@
-import geopandas as gpd
-
-path = r"C:\Users\rdh08\Desktop\2026_Data_Visualization\Dataset\BND_ADM_DONG_PG\BND_ADM_DONG_PG.shp"
-gdf = gpd.read_file(path)
-# gdf.to_file("seongnam_dong.geojson", driver="GeoJSON")
-
-print(gdf.head())
